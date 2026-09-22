272e8486233f337c77d6c9ae75577feb3c304bfa
friscodd <friscoarciady@gmail.com>
2026-09-22 13:38:50 +0700

    2 commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/pokedex-mini/', // replace with your own repo name
   plugins: [react()],
 })
